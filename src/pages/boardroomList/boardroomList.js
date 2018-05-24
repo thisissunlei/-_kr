2943f4710bb481be30ec42a45ea326a922600619
张弛 <zhangchi@krspace.cn>
2018-05-24 21:05:45 +0800

    fix:

--- src/pages/boardroomList/boardroomList.js
+++ src/pages/boardroomList/boardroomList.js
@@ -8,7 +8,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    boardroomList:[{roomName:'A会议室',imgUrl:'../images/boardroomList/guding.png',capacity:'3',floor:'03F'},{roomName:'B会议室',imgUrl:'../images/boardroomList/duli.png',capacity:'3',floor:'03F'},{roomName:'C会议室',imgUrl:'../images/boardroomList/duli.png',capacity:'3',floor:'03F'},{roomName:'D会议室',imgUrl:'../images/boardroomList/guding.png',capacity:'3',floor:'03F'}],
+    boardroomList:[{roomName:'A会议室',imgUrl:'../images/boardroomList/guding.png',capacity:'3',floor:'03F',device:[{name:'电视'},{name:'空调'},{name:'洗衣机'}],unitCost:'60'},{roomName:'B会议室',imgUrl:'../images/boardroomList/duli.png',capacity:'3',floor:'03F',device:[{name:'电视'},{name:'空调'},{name:'洗衣机'}],unitCost:'60'}],
   },
   //事件处理函数
   bindViewTap: function() {
