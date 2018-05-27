99e25bf1152a0628f8538387241318bf490da169
dongfanai <dongfanai@krspace.cn>
2018-05-27 16:26:03 +0800

    feat: 黄色底线

--- src/pages/orderConfirmation/orderConfirmation.js
+++ src/pages/orderConfirmation/orderConfirmation.js
@@ -1,12 +1,23 @@
 //orderConfirmation.js
 //获取应用实例
 const app = getApp()
+var newDate=new Date();
+var theme=newDate;
+
 
 Page({
   data: {
     motto: 'Hello World',
+    theme:'0510会议',
   },
-  
+  //事件处理函数
+  jumpSetTheme:function() {
+      console.log('---->>>>',this.data)
+    // wx.navigateTo({
+    //   url: '../meetingTheme/meetingTheme?value='+meetingTheme
+    // })
+   
+  },
   onLoad: function (options) {
    
   },
