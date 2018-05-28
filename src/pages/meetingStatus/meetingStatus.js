f8bd64b159da59d59b0b17b3dc6a303801aade5c
费姗姗 <zhanfeng@krspace.cn>
2018-05-28 10:20:25 +0800

    新建会议状态

---
+++ src/pages/meetingStatus/meetingStatus.js
@@ -0,0 +1,54 @@
+//index.js
+//获取应用实例
+var QR = require("../../utils/qrcode.js");
+
+const app = getApp()
+
+Page({
+  data: {
+    conferee:[
+      {
+        'name':'刘佳佳'
+      },
+      {
+        'name':'molly'
+      },
+      {
+        'name':'谭烨'
+      },
+      {
+        'name':'沈美美'
+      },
+      {
+        'name':'段郝耀'
+      },
+      {
+        'name':'蒋萌'
+      }
+    ],
+    hint:[
+      {
+        'title':'到了如何使用会议室？',
+        'text':'您到现场出示二维码认证成功后，运营人员会帮您打开会议室门哦~'
+      },
+      {
+        'title':'到了如何到了使用到了会议室？',
+        'text':'您到现场出示二维码认证成功后，运营人员会帮您打开会议室门哦~'
+      },
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.createQrCode('fdfd',"mycanvas",100,200);
+  },
+  createQrCode:function(url,canvasId,cavW,cavH){
+    //调用插件中的draw方法，绘制二维码图片
+    QR.qrApi.draw(url,canvasId,cavW,cavH);
+    var that = this;
+  }
+})
