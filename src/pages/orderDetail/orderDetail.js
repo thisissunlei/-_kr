8d1311db38e9d568d88d507c819491115e6b80cb
dongfanai <dongfanai@krspace.cn>
2018-05-28 15:03:09 +0800

    feat: 订单详情

--- src/pages/orderDetail/orderDetail.js
+++ src/pages/orderDetail/orderDetail.js
@@ -16,16 +16,66 @@ Page({
     console.log(CAlculagraph)
     const time = CAlculagraph.CAlculagraph();
     const that = this;
-    time.timerMint({
-      deadline:new Date().getTime()/1000+300,//最终结束的时间戳,
-      callback:function (){
-        console.log(111)
-      },//时间结束
-      that:this
-    });
+    // time.timerMint({
+    //   deadline:new Date().getTime()/1000+300,//最终结束的时间戳,
+    //   callback:function (){
+    //     console.log(111)
+    //   },//时间结束
+    //   that:this
+    // });
+    this.getDetailInfo('2')
+   
+  },
+  getDetailInfo:function(orderId){
+
+    let data={
+      orderShowStatus:2
+    }
+    let titleObj={
+      '1':'待支付订单',
+      '2':'待使用订单',
+      '3':'已使用订单',
+      '4':'已取消订单'
+    }
+
+
     wx.setNavigationBarTitle({
-      title: '当前页面'
+      title: titleObj[data.orderShowStatus]
     })
-  }
+
+
+
+    wx.request({
+      url:'/api/gateway/krmting/order/detail',
+      methods:"GET",
+      header:{
+        'content-type':"appication/json"
+      },
+      data:{
+        orderId:orderId
+      },
+      success:(res)=>{
+          let data=res.data;
+            console.log(res)
+            // this.setData({
+            //   count:res.data.count
+            // })
+            let titleObj={
+              '1':'待支付订单',
+              '2':'待使用订单',
+              '3':'已使用订单',
+              '4':'已取消订单'
+            }
+
+
+            wx.setNavigationBarTitle({
+              title: titleObj[data.orderShowStatus]
+            })
+      }
+  })
+  
+  
+  },
+
    
 })
