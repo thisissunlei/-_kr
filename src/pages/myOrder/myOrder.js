b2909d15dd4fa571841603f1807c3a372d300016
马超越 <machaoyue@36kr.com>
2018-05-29 15:37:50 +0800

    fix:

--- src/pages/myOrder/myOrder.js
+++ src/pages/myOrder/myOrder.js
@@ -3,6 +3,8 @@
 const app = getApp()
 Page({
   data: {
+    minute:'',
+    second:'',
     orderList:[
       {
         "buildName":"测试内容6n27",
@@ -56,16 +58,51 @@ Page({
     type:'',
     number:'0'
   },
+  startcountDate:function(){
+    const time = CAlculagraph.CAlculagraph();
+    const that = this;
+    time.timerMint({
+      deadline:new Date().getTime()/1000+300,//最终结束的时间戳,
+      callback:function (){
+        console.log(111)
+      },//时间结束
+      that:this
+    });
+  },
   changeType:function(e){
-    console.log('changeType====',e.target.dataset)
+    let that = this;
     let data = e.target.dataset;
     let type = data.type;
     this.setData({
       number:20*type + '%',
       type:type
+    },function(){
+      that.getData(type)
     })
   },
   onLoad: function () {
-   
+    this.getData('0')
   },
+  getData:function(type){
+    let that = this;
+    wx.request({
+        url:app.globalData.KrUrl+'/api/gateway/krmting/order/list',
+        methods:"GET",
+        header:{
+          'content-type':"appication/json"
+        },
+        data:{
+          orderShowStatus:type
+        },
+        success:(res)=>{
+          cosole.log(res)
+          // that.setData({
+          //   orderList:res.data
+          // })
+        },
+        fail:(res)=>{
+           console.log('========',res)
+        }
+      })
+  }
 })
