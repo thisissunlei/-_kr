41ecb162d96209c2bd29feea6a1c6e91e9b26281
dongfanai <dongfanai@krspace.cn>
2018-05-30 11:53:37 +0800

    index

--- src/pages/orderDetail/orderDetail.js
+++ src/pages/orderDetail/orderDetail.js
@@ -99,7 +99,7 @@ Page({
 
     
    
-    //this.getDetailInfo('2')
+    this.getDetailInfo('2')
    
   },
   getDetailInfo:function(orderId){
@@ -107,15 +107,12 @@ Page({
     app.getRequest({
         url:app.globalData.KrUrl+'api/gateway/krmting/order/detail',
         method:"GET",
-        header:{
-          'content-type':"appication/json"
-        },
         data:{
           orderId:orderId
         },
         success:(res)=>{
             let data=res.data;
-              console.log(res)
+              console.log('res----',res)
             
               let titleObj={
                 '1':'待支付订单',
@@ -150,6 +147,9 @@ Page({
                 title: titleObj[data.orderShowStatus]
               })
               _this.startcountDate(detailInfo.ctime);
+        },
+        fail:(error)=>{
+          consolr.log('error----',error)
         }
       })
   },
