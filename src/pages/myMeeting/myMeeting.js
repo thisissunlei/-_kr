be5cedfe4605e024fb6042573313e5ea12402ec0
马超越 <machaoyue@36kr.com>
2018-05-30 18:58:11 +0800

    fix:

--- src/pages/myMeeting/myMeeting.js
+++ src/pages/myMeeting/myMeeting.js
@@ -21,22 +21,22 @@ Page({
         status:1,
         id:13760
       },
-      {
-        meetingTime:'11-29 (周三)  9:00-10:30',
-        themeName:'测试内容u19h',
-        meetingRoomName:'测试内容测试内容测试内容测试试测试内容测试内容123',
-        address:'测试内容u19h',
-        status:2,
-        id:13761
-      },
-      {
-        meetingTime:'11-29 (周三)  9:00-10:30',
-        themeName:'测试内容u19h',
-        meetingRoomName:'测试内容u19h',
-        address:'测试内容u19h',
-        status:3,
-        id:13762
-      },
+      // {
+      //   meetingTime:'11-29 (周三)  9:00-10:30',
+      //   themeName:'测试内容u19h',
+      //   meetingRoomName:'测试内容测试内容测试内容测试试测试内容测试内容123',
+      //   address:'测试内容u19h',
+      //   status:2,
+      //   id:13761
+      // },
+      // {
+      //   meetingTime:'11-29 (周三)  9:00-10:30',
+      //   themeName:'测试内容u19h',
+      //   meetingRoomName:'测试内容u19h',
+      //   address:'测试内容u19h',
+      //   status:3,
+      //   id:13762
+      // },
     ]
   },
   //事件处理函数
@@ -56,7 +56,7 @@ Page({
         success:(res)=>{
           console.log(res)
           // that.setData({
-          //   meetingList:res.data
+          //   meetingList:res.data.data.items
           // })
         },
         fail:(res)=>{
