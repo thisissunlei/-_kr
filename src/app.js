d0bfaef7bab8f467ddb9437a7b1424dc59cb5f83
战锋 <feng.zhan@moji.com>
2018-06-01 18:16:37 +0800

    xiugai首页权限

--- src/app.js
+++ src/app.js
@@ -45,6 +45,9 @@ App({
   },
   getRequest:function (data){
     const that = this;
+    if(data.url.indexOf('api/gateway/krmting/user/save')>0){
+      data.data['openid'] = this.globalData.openid;
+    }
     wx.request({
       url: data.url,
       method:data.method||'get',
