a8c4eece4df7a11125cc65e63d0b4d7cb8760e41
张弛 <zhangchi@krspace.cn>
2018-06-01 16:22:39 +0800

    fix:

--- src/pages/orderConfirmation/orderConfirmation.js
+++ src/pages/orderConfirmation/orderConfirmation.js
@@ -71,6 +71,13 @@ Page({
         dialogShow:!this.data.dialogShow
     })
   },
+  currentChange:function(e){
+    if(e.detail.source=="touch"){
+      this.setData({
+        currentNum:e.detail.current+1
+      })
+    }
+  },
   openMeetDetail:function(e){
     let that = this;
     this.setData({
@@ -89,7 +96,7 @@ Page({
             that.setData({
               meetingRoomId:res.data.meetingRoomId
             },function(){
-              that.getMeetDetail()
+              that.getMeetDetail();
             })
           }
         }
@@ -270,7 +277,7 @@ Page({
   onLoad: function (options) {
     // var rangeTime = wx.getStorageSync('rangeTime');
     this.getIsfirst();
-    
+    this.getPhone();
     var _this=this;
     wx.getStorage({
       key:'detail',
@@ -330,24 +337,20 @@ Page({
     })
 
 
-
-
-    var rangeTime = wx.getStorageSync('rangeTime',).map((item,index)=>{
-      // if (index==indexParam) {
-        // item.actived = true; 
-      // }else{
-        item.actived = false; 
-      // }
-      return item;
-    })
-    console.log(rangeTime);
-    this.setData({
-      rangeTime1:rangeTime.slice(0,8),
-      rangeTime2:rangeTime.slice(8,16),
-      rangeTime3:rangeTime.slice(16),
-      rangeTime:rangeTime,
-      nowDate:wx.getStorageSync('nowDate')
-    })
+    this.getMeetId();
+    
+    // var rangeTime = wx.getStorageSync('rangeTime',).map((item,index)=>{
+    //   item.actived = false; 
+    //   return item;
+    // })
+    // console.log(rangeTime);
+    // this.setData({
+    //   rangeTime1:rangeTime.slice(0,8),
+    //   rangeTime2:rangeTime.slice(8,16),
+    //   rangeTime3:rangeTime.slice(16),
+    //   rangeTime:rangeTime,
+    //   nowDate:wx.getStorageSync('nowDate')
+    // })
     
     
   },
@@ -388,8 +391,37 @@ Page({
   },
   closeDialogTime:function(){
     var that = this;
+    if(!that.data.dialogTimeShow){
+      that.getMeetDetail();
+      that.getNowRangeTime();
+    }
     this.setData({
       dialogTimeShow:!that.data.dialogTimeShow
+    })
+  },
+  getNowRangeTime:function(){
+    var meetingDetail = Object.assign({},this.data.meetingDetail);
+    var disableTime = meetingDetail.disableTime;
+    console.log(meetingDetail);
+    var newRangeTime = [];
+    for (let i = 19; i < 39; i++) {
+      var rangeTimeItem = {
+        disabled:false,
+        number: i
+      };
+      newRangeTime.push(rangeTimeItem);
+    }
+    newRangeTime.forEach((timeItem,timeIndex) => {
+        if(disableTime.indexOf(timeItem.number)>-1){
+            timeItem.disabled = true;
+        }
+    });
+    this.setData({
+      rangeTime1:newRangeTime.slice(0,8),
+      rangeTime2:newRangeTime.slice(8,16),
+      rangeTime3:newRangeTime.slice(16),
+      rangeTime:newRangeTime,
+      nowDate:wx.getStorageSync('nowDate')
     })
   },
   goToPay:function(){
@@ -515,9 +547,9 @@ Page({
             },
             'fail':function(response){
               console.log('response-----',response)
-                // wx.navigateTo({
-                //   url: '../orderDetail/orderDetail?id='+data.orderId
-                // })
+                wx.navigateTo({
+                  url: '../orderDetail/orderDetail?id='+data.orderId
+                })
             },
            
           })
@@ -546,6 +578,7 @@ Page({
             that.setData({
               meetingDetail:meetingDetail
             })
+            that.getNowRangeTime();
           }else{
             that.setData({
               phoneError:false,
