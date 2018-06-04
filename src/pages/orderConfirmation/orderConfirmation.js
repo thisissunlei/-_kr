d4695fc51536157a395bd0c9ecaa7b16a6c7d1e0
dongfanai <dongfanai@krspace.cn>
2018-06-04 17:38:33 +0800

    feat:

--- src/pages/orderConfirmation/orderConfirmation.js
+++ src/pages/orderConfirmation/orderConfirmation.js
@@ -146,23 +146,24 @@ Page({
         }
       }
       let Time="meeting_time.time";
+      
       this.setData({
         orderDate:{
           time:time,
           timeText:day_con,
         },
         [Time]:'',
-        newDate:time,
+        nowDate:time,
         priceCount:0,
         totalCount:0
-       
         
       },function(){
-        console.log('orderDate---->>>',_this.data.orderDate)
+       
         _this.closeDialogDate();
         _this.getThemeName(_this.data.orderDate);
        
       })
+      
 
       
 
@@ -443,6 +444,7 @@ Page({
     }
     var that = this;
     //console.log("all",selectedTime);
+    console.log('that.data.nowDate+',that.data.nowDate)
     if(bool){
       this.setData({
         rangeTime1:[].concat(rangeTime.slice(0,8)),
@@ -809,6 +811,7 @@ Page({
       meetingRoomId:data.detailInfo.meetingRoomId,
       themeName:data.order_pay.themeName || data.themeName
     }
+    
     var _this=this;
         app.getRequest({
           url:app.globalData.KrUrl+'api/gateway/krmting/order/create',
