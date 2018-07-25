cd9a2634c9a392eefff82840ef1e529b8ece46b3
wangboxin <1366580432@qq.com>
2018-07-26 02:01:29 +0800

    日历修改

--- src/pages/seatorderConfirmation/seatorderConfirmation.js
+++ src/pages/seatorderConfirmation/seatorderConfirmation.js
@@ -144,15 +144,8 @@ Page({
   },
   // 数量日历显示与隐藏
    closeDialogDate:function(){
-<<<<<<< HEAD
      this.setData({
-       show_a:false
-=======
-    //  console.log(this.data.id)
-     wx.navigateTo({
-       url:"../meeting/meeting?seatId="+this.data.seatId
->>>>>>> b860c284b378e3059e885ca2ee25ce97377858e0
-     })
+       show_a:false })
     //  console.log(this.data.id)
     //  wx.redirectTo({
     //    url:"../meeting/meeting?seatId="+this.data.seatId
@@ -393,23 +386,23 @@ Page({
     console.log(1111)
     wx.setStorageSync('data-index',this.data.combination)
     // console.log(this.data.id)
-    app.getRequest({
-      url:app.globalData.KrUrl+'api/gateway/krseat/seat/goods/list',
-      methods:"GET",
-      data:{
-        seatId:this.data.id
-      },
-      success:res=>{
+    // app.getRequest({
+    //   url:app.globalData.KrUrl+'api/gateway/krseat/seat/goods/list',
+    //   methods:"GET",
+    //   data:{
+    //     seatId:this.data.id
+    //   },
+    //   success:res=>{
         // console.log(res)
-        let ss = this.data.id
+        // let ss = this.data.id
         // setTimeout(function(){
         //   wx.navigateTo({
         //     url:"/pages/seatorderConfirmation/seatorderConfirmation?id="+ss
         //   })
         //     },0)
 
-      }
-    })
+    //   }
+    // })
     this.onClickDate(that);
     this.setData({
       show_a:true
@@ -931,8 +924,13 @@ Page({
             
           }
           this.all_day_num++;
+          console.log(this.all_day_num)
+          console.log(30-this.all_day_num+week)
           break;
+          
         case i<(30-this.all_day_num+week)&&!bool:
+        console.log(this.all_day_num)
+        console.log(30-this.all_day_num+week)
           if(i%7==0||i%7==6){ 
             // console.log(i+"下月星期天")
             // console.log(this.data.arr1)
@@ -942,8 +940,9 @@ Page({
               name:"周天"
             });
           }else{
+            // console.log(index_zhu.arr1)
             console.log(i-week)
-            console.log(index_zhu.arr1[i-week],111111111111111)
+            // console.log(index_zhu.arr1[i-week],111111111111111)
             if(index_zhu.arr1[i-week].remainQuantity > 1 && index_zhu.arr1[i-week].remainQuantity<index_zhu.number){
               data.push({//除周六日可选
                 value:i-week+1,
