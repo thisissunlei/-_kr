97863aacd2836696f2154410215366cc8407d627
lifan <lifan@gongsibao.com>
2018-08-10 15:21:13 +0800

    优惠券列表UI

--- src/pages/myCoupon/myCoupon.js
+++ src/pages/myCoupon/myCoupon.js
@@ -3,7 +3,8 @@
 const app = getApp()
 Page({
     data: {
-        list: [{id: 0}, {id: 1}, {id: 2}]
+        list: [{id: 0, state: true, price: 10}, {id: 1, state: true, price: 88}, {id: 2, state: false, price: 100}],
+        list2: [{id: 10, state: true, price: 50}, {id: 11, state: true, price: 888}, {id: 12, state: false, price: 1000}]
     },
     onShow: function () {
 
