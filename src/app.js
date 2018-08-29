30a4ebd6d4760601e50dfbf2a265fc53518a096d
xinxueliang <188078027@qq.com>
2018-08-29 16:24:59 +0800

    feat:ui样式修改

--- src/app.js
+++ src/app.js
@@ -39,8 +39,8 @@ App({
   },
   globalData: {
     userInfo: null,
-    KrUrl: "https://i.krspace.cn/",
-    Cookie: "", 
+    KrUrl: "https://i.krspace.cn/test02/",
+    Cookie: "",
     share_data: {
       title: "开启轻松、灵活办公新方式",
       desc: "氪空间自由座",
