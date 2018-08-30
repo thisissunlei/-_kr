e1f062545763479a8610e4b9c5f8b2ddd984ccc6
dongfanai <dongfanai@krspace.cn>
2018-08-30 15:32:00 +0800

    feat: bg

--- src/pages/insideSale/insideSale.js
+++ src/pages/insideSale/insideSale.js
@@ -1,10 +1,13 @@
 
+//获取应用实例
+const app = getApp();
+
 Page({
   data: {
-    
+    imgUrl:app.globalData.KrImgUrl 
   },
   onLoad: function () {
-    
+    console.log('app.globalData.KrImgUrl ',app.globalData.KrImgUrl)
   },
  
  
