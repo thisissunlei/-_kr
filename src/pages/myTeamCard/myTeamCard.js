2ae55d33212d34728af9eb77b54cc8d353e03bf2
xinxueliang <188078027@qq.com>
2018-09-03 12:06:25 +0800

    feat:我的团队卡

--- src/pages/myTeamCard/myTeamCard.js
+++ src/pages/myTeamCard/myTeamCard.js
@@ -15,7 +15,7 @@ Page({
         name: "多块好省闪卡",
         type: "2",
         number: "NO.123456",
-        coust: 5000,
+        coust: 50000,
         time: "使用期限：2018.09.10-2019.09.09",
         stause: "unused",
         id: 2
@@ -33,16 +33,17 @@ Page({
         name: "速决闪卡",
         type: "2",
         number: "NO.123456",
-        coust: 0,
+        coust: 99,
         time: "使用期限：2018.09.10-2019.09.09",
         stause: "ex",
         id: 4
       }
     ]
   },
-  onLoad: function() {
-    this.getTeamCard();
-  },
+  onLoad: function() {},
+  onReady: function() {},
+  //购买团队卡
+  goBuyTeamCard: function() {},
   //详情页跳转
   jumpCardDetail: function() {},
   //我的团队卡接口
@@ -57,5 +58,18 @@ Page({
         console.log(err);
       }
     });
+  },
+  //金额格式化
+  toThousands: function(num) {
+    var num = (num || 0).toString(),
+      result = "";
+    while (num.length > 3) {
+      result = "," + num.slice(-3) + result;
+      num = num.slice(0, num.length - 3);
+    }
+    if (num) {
+      result = num + result;
+    }
+    return result;
   }
 });
