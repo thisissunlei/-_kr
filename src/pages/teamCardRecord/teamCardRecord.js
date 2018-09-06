055ba83c1614df8c0f40801c07c265f63729a810
lifan <lifan@gongsibao.com>
2018-09-06 16:11:34 +0800

    消费记录

--- src/pages/teamCardRecord/teamCardRecord.js
+++ src/pages/teamCardRecord/teamCardRecord.js
@@ -1,27 +1,93 @@
 const app = getApp()
 Page({
-    data: {},
-    onLoad() {},
+    data: {
+        list: [],
+        loading: true
+    },
+    usedlistData: {
+        cardId: null,
+        page: 1,
+        pageSize: 10
+    },
+    totalPages: 0,
     down: false,
+    onLoad(options) {
+        this.usedlistData.cardId = options.cardId
+        wx.showLoading({
+            mask: true
+        });
+        this.getUsedlist()
+    },
+    getUsedlist() {
+        app.getRequest({
+            url: app.globalData.KrUrl + "api/gateway/kmteamcard/teamcard/usedlist",
+            methods: "GET",
+            data: this.usedlistData,
+            success: res => {
+                this.totalPages = res.data.data.totalPages
+                if ( !!res.data.data.items && res.data.data.items.length > 0 ) {
+                    res.data.data.items.forEach((val, i) => {
+                        val.day = this.setTime(val.ctime, 'day')
+                        val.time = this.setTime(val.ctime, 'time')
+                        val.amount = val.amount.toString().replace(/(\d{1,3})(?=(\d{3})+$)/g,'$1,')
+                    })
+                    this.setData({
+                        list: [].concat(this.data.list, res.data.data.items),
+                        loading: false
+                    })
+                } else {
+                    this.setData({
+                        list: [].concat(this.data.list, []),
+                        loading: false
+                    })
+                }
+                wx.hideLoading()
+                if ( this.down ) {
+                    wx.stopPullDownRefresh()
+                    this.down = false
+                }
+            },
+            fail: res => {}
+        });
+    },
     // 下拉刷新
     onPullDownRefresh(e) {
         if ( !!this.down ) return
         this.down = true
         wx.startPullDownRefresh({
             success: () => {
-                this.getData()
+                this.usedlistData.page = 1
+                wx.showLoading({
+                    mask: true
+                });
+                this.setData({
+                    list: [],
+                    loading: true
+                }, () => {
+                    this.getUsedlist()
+                })
             }
         })
     },
     // 上拉加载
     onReachBottom() {
-        console.log('000')
+        if ( !this.totalPages || this.totalPages >= this.usedlistData.page ) return
+        this.usedlistData.page = this.usedlistData.page+1
+        this.getUsedlist()
     },
-    getData() {
-        // 停止动画
-        setTimeout(() => {
-            wx.stopPullDownRefresh()
-            this.down = false
-        }, 2000)
+    setTime(time, state) {
+        var y, M, d, h, m, s
+        y = new Date(parseInt(time)).getFullYear();
+        M = new Date(parseInt(time)).getMonth() + 1 >= 10 ? new Date(parseInt(time)).getMonth() + 1 : "0" + (new Date(parseInt(time)).getMonth() + 1);
+        d = new Date(parseInt(time)).getDate() >= 10 ? new Date(parseInt(time)).getDate() : "0" + new Date(parseInt(time)).getDate();
+
+        h = new Date(parseInt(time)).getHours() >= 10 ? new Date(parseInt(time)).getHours() : "0" + new Date(parseInt(time)).getHours();
+        m = new Date(parseInt(time)).getMinutes() >= 10 ? new Date(parseInt(time)).getMinutes() : "0" + new Date(parseInt(time)).getMinutes();
+        s = new Date(parseInt(time)).getSeconds() >= 10 ? new Date(parseInt(time)).getSeconds() : "0" + new Date(parseInt(time)).getSeconds();
+        if ( state === 'day' ) {
+            return  M + '-' + d
+        } else {
+            return h +':'+ m +':'+ s
+        }
     }
 })
