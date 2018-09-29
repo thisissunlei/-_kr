77a5c12f3c1b185c2d601b439abf34abe0c7fc41
xinxueliang <188078027@qq.com>
2018-09-29 17:44:33 +0800

    feat:助力活动详情

--- src/pages/createImg/createImg.js
+++ src/pages/createImg/createImg.js
@@ -1,126 +1,153 @@
 //index.js
-import Poster from '../wxa-plugin-canvas/poster/poster';
+import Poster from "../wxa-plugin-canvas/poster/poster";
 const app = getApp();
 Page({
-    data: {
-        showSuccess:false,
-        KrImgUrl:app.globalData.KrImgUrl ,
-        imgUrl:'',
-        showShare:false,
-        
-    },
-    jdConfig: {
+  data: {
+    showSuccess: false,
+    KrImgUrl: app.globalData.KrImgUrl,
+    imgUrl: "",
+    showShare: false,
+    showRule: false, //活动规则
+    noticeList: [
+      { text: "猫力大大美 3分钟前 提取了 30元礼券" },
+      { text: "猫力大大美 4分钟前 提取了 50元礼券" },
+      { text: "猫力大大美 5分钟前 提取了 100元礼券" }
+    ]
+  },
+  jdConfig: {
+    width: 765,
+    height: 1068,
+    backgroundColor: "#fff",
+    debug: false,
+    images: [
+      {
         width: 765,
         height: 1068,
-        backgroundColor: '#fff',
-        debug: false,
-        images: [
+        x: 1,
+        y: 0,
+        borderRadius: 16,
+        url: "/pages/images/shareBg.png",
+        zIndex: 2
+      }
+    ]
+  },
+  weImg: {
+    width: 486,
+    height: 486,
+    x: 141,
+    y: 390,
+    url: "",
+    zIndex: 1
+  },
+  //转发分享
+  onShareAppMessage: function(res) {
+    // wx.reportAnalytics("sharekrmeeting");
+    // console.log(res);
+  },
+  //活动规则
+  helpingRule: function() {
+    this.setData({
+      showRule: true
+    });
+  },
+  //关闭活动规则
+  closeRule: function() {
+    this.setData({
+      showRule: false
+    });
+  },
+  //跳转我的礼品券
+  jumpMyCoupon: function() {
+    wx.navigateTo({
+      url: "../myCoupon/myCoupon"
+    });
+  },
+  closeDialog() {
+    this.setData({
+      showShare: false,
+      showSuccess: false
+    });
+  },
+  saveImg() {
+    //保存图片到本地
+    let that = this;
+    // wx.saveImageToPhotosAlbum({
+    //     filePath: that.data.imgUrl,
+    //     success:function(res){
+    //         console.log('success',res)
+    that.setData({
+      showShare: false,
+      showSuccess: true
+    });
+    //     },
+    //     fail:function(res){
+    //         console.log('fail',res)
+    //     }
+    // }, this)
+    //保存图片到本地--end
+  },
+  onPosterSuccess(e) {
+    const { detail } = e;
+    wx.hideLoading();
+    this.setData({
+      imgUrl: detail,
+      showShare: true
+    });
+  },
+  onPosterFail(err) {
+    console.error(err);
+  },
+
+  createShareCanvas() {
+    let weImg = this.weImg;
+    let jdConfig = this.jdConfig;
+    let that = this;
+    wx.showLoading({
+      title: "加载中"
+    });
+
+    app.getRequest({
+      url: app.globalData.KrUrl + "api/gateway/kmbooster/promocode",
+      data: {
+        page: "pages/activityDetails/activity",
+        scene: "8"
+      },
+      success: res => {
+        let code = res.data.code;
+        if (code === 1) {
+          // weImg.url = res.data.data
+          weImg.url = "/pages/images/shi.jpg";
+          jdConfig.images.push(weImg);
+          this.setData(
             {
-                width: 765,
-                height: 1068,
-                x: 1,
-                y: 0,
-                borderRadius: 16,
-                url: '/pages/images/shareBg.png',
-                zIndex:2
+              jdConfig: jdConfig
             },
-        ]
-    },
-    weImg:{
-        width: 486,
-        height: 486,
-        x: 141,
-        y: 390,
-        url: '',
-        zIndex:1
-    },
-    closeDialog(){
-        this.setData({
-            showShare:false,
-            showSuccess:false
-        })
-    },
-    saveImg(){
-        //保存图片到本地
-        let that = this;
-        // wx.saveImageToPhotosAlbum({
-        //     filePath: that.data.imgUrl,
-        //     success:function(res){
-        //         console.log('success',res)
-                that.setData({
-                    showShare:false,
-                    showSuccess:true
-                })
-        //     },
-        //     fail:function(res){
-        //         console.log('fail',res)
-        //     }
-        // }, this)
-        //保存图片到本地--end
-    },
-    onPosterSuccess(e) {
-        const { detail } = e;
-        wx.hideLoading();
-        this.setData({
-            imgUrl:detail,
-            showShare:true
-        })
-    },
-    onPosterFail(err) {
-        console.error(err);
-    },
-    
-    createShareCanvas(){
-        let weImg = this.weImg
-        let jdConfig = this.jdConfig
-        let that = this;
-        wx.showLoading({
-          title: "加载中"
-        });
-
-
-        app.getRequest({
-          url: app.globalData.KrUrl + "api/gateway/kmbooster/promocode",
-          data:{
-            page:'pages/activityDetails/activity',
-            scene:'8'
-          },
-          success: res => {
-            let code = res.data.code;
-            if(code===1){
-                // weImg.url = res.data.data
-                weImg.url = '/pages/images/shi.jpg'
-                jdConfig.images.push(weImg)
-                this.setData({
-                    jdConfig:jdConfig,
-                },function(){
-                    Poster.create();
-                    
-                })
-            }else{
-                // weImg.url = res.data.data
-                weImg.url = 'https://img.krspace.cn/activity/image/0/2018/09/25/115630761C2e8epT.jpg'
-                jdConfig.images.push(weImg)
-                this.setData({
-                    jdConfig:jdConfig,
-                },function(){
-                    Poster.create();
-                    
-                })
+            function() {
+              Poster.create();
             }
-            
+          );
+        } else {
+          // weImg.url = res.data.data
+          weImg.url =
+            "https://img.krspace.cn/activity/image/0/2018/09/25/115630761C2e8epT.jpg";
+          jdConfig.images.push(weImg);
+          this.setData(
+            {
+              jdConfig: jdConfig
+            },
+            function() {
+              Poster.create();
+            }
+          );
+        }
+      }
+    });
+  },
 
-          }
-        })
-
-        
-           
-    },
-
-    /**
-     * 异步生成海报
-     */
-    onCreatePoster() {
-        Poster.create();
-    }
-})
+  /**
+   * 异步生成海报
+   */
+  onCreatePoster() {
+    Poster.create();
+  }
+});
