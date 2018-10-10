4bbf82e7a302e36c0f5cd9bc621d51f3d33a8192
185355787@qq.com <185355787@qq.com>
2018-10-10 19:07:09 +0800

    添加动画

--- src/pages/assistance/assistance.js
+++ src/pages/assistance/assistance.js
@@ -1,4 +1,5 @@
 const app = getApp()
+import {demoAnimate,demoAnimates} from '../../utils/animate.js';
 Page({
     data: {
         assistance:{},
@@ -13,10 +14,23 @@ Page({
         totalAmount:'',
         totalCount:'',
         items:[],
-
+        numArr:[{label:'0'},{label:'0'}],
+        numArrs:[{label:'0'},{label:'0'}],
+        number:'520',
+        KrImgUrl: app.globalData.KrImgUrl, //CDN图片路径
     },
+    aaa :3445,
+    james:'',
+    other:'',
     onLoad(options) {
       let that = this;
+      let numArr = this.data.numArr;
+      let numArrs = this.data.numArrs;
+      this.james = new demoAnimates({
+            numArr:numArr,
+            _this:that,
+            callback:function(that){console.log('ok',that,that.aaa)}
+          });
       wx.getSetting({
         success: res => {
           if (res.authSetting["scope.userInfo"]) {
