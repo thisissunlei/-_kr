b748d31510f0e62871048ee46b36b26942c4b792
xinxueliang <188078027@qq.com>
2018-10-10 18:10:48 +0800

    feat:引导页

---
+++ src/pages/laymanGuide/laymanGuide.js
@@ -0,0 +1,24 @@
+const app = getApp();
+Page({
+  data: {
+    KrImgUrl: app.globalData.KrImgUrl, //CDN图片路径
+    imgUrls: [
+      "/image/u25.png",
+      "/image/u27.png",
+      "/image/u29.png",
+      "/image/u24.png"
+    ],
+    swiperCurrent: 0
+  },
+  swiperChange: function(e) {
+    // console.log(e);
+    this.setData({
+      swiperCurrent: e.detail.current //获取当前轮播图片的下标
+    });
+  },
+  jumpToIndex: function() {
+    wx.reLaunch({
+      url: "../index/index"
+    });
+  }
+});
