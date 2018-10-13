df0722f259776088b350765cf25e80ac2b37ddfe
machaoyue <machaoyue@36kr.com>
2018-10-13 21:53:11 +0800

    fix: 9999

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -1,6 +1,7 @@
 //获取应用实例
 const app = getApp();
-import {demoAnimate,demoAnimates} from '../../utils/animate.js';
+import {demoAnimate,demoAnimates} from '../../utils/numAnimate.js';
+// import {demoAnimate,demoAnimates} from '../../utils/animate.js';
 let _this 
 Page({
   data: {
@@ -26,83 +27,30 @@ Page({
     let numArrs = this.data.numArrs;
   },
   onReady:function(){
-    console.log('onReady---')
     let ii = 0
     let that = this;
-
-    // // 第一个
-    // var animationData = wx.createAnimation({
-    //   duration: 400, // 默认为400     动画持续时间，单位ms
-    //   // timingFunction: 'ease-in-out',
-    //   //transformOrigin: '4px 91px'
-    // });
-    // var animationDataTwo = wx.createAnimation({
-    //   duration: 400, // 默认为400     动画持续时间，单位ms
-    //   // timingFunction: 'ease-in-out',
-    //   //transformOrigin: '4px 91px'
-    // });
-    // animationData.translateY(-670).step({ duration: 2000 }).translateY(603).step({ duration: 200 }).translateY(0).step({ duration: 1800 });
-    // animationDataTwo.translateY(-670).step({ duration: 2000 }).translateY(603).step({ duration: 200 }).translateY(0).step({ duration: 1800 });
-
-    // // 第二个
-    // var animationCloudData = wx.createAnimation({
-    //   duration: 400, // 默认为400     动画持续时间，单位ms
-    //   // duration: 1000, // 默认为400     动画持续时间，单位ms
-    //   // timingFunction: 'ease-in-out',
-    // });
-    // animationCloudData.translateY(-670).step({ duration: 2000 }).translateY(-1340).step({ duration: 2000 }).translateY(0).step({ duration: 100 });
-    // this.setData({
-    //   // 导出动画示例
-    //   animationCloudData: animationCloudData.export(),
-    //   animationDataOne: animationData.export(),
-    //   animationDataTwo: animationDataTwo.export(),
-      
-    // },function(){
-    //   setTimeout(function(){
-        
-    //   },4100)
-    // })
-    this.WXAniamtion()
+    // this.WXAniamtion()
+    this.james = new demoAnimates({
+      _this:that,
+      callback:function(){
+        console.log('callback',that.aaa)
+      }
+    })
     
-    
-    // if(run){
-    //   this.james = setTimeout(function () {
-        //动画的脚本定义必须每次都重新生成，不能放在循环外
-      
-        
-      //   // 更新数据
-      //   that.setData({
-      //     // 导出动画示例
-      //     animationData: animationData.export(),
-      //     animationCloudData: animationCloudData.export(),
-      //   })
-      //   ++ii;
-      //   console.log(ii);
-
-      // }.bind(that),4200);
-
-    // }
+   
   },
   WXAniamtion(){
     let that = this;
-    console.log('WXAniamtion')
      var animationData = wx.createAnimation({
-      duration: 4100, // 默认为400     动画持续时间，单位ms
-      // timingFunction: 'ease-in-out',
-      //transformOrigin: '4px 91px'
+      duration: 400,
     });
      var animationDataTwo = wx.createAnimation({
-      duration: 400, // 默认为400     动画持续时间，单位ms
-      // timingFunction: 'ease-in-out',
-      //transformOrigin: '4px 91px'
+      duration: 400, 
     });
     // 第二个
     var animationCloudData = wx.createAnimation({
-      duration: 4100, // 默认为400     动画持续时间，单位ms
-      // duration: 1000, // 默认为400     动画持续时间，单位ms
-      // timingFunction: 'ease-in-out',
+      duration: 400, 
     });
-    let i = 0
     animationData.translateY(-670).step({ duration: 2000 }).translateY(603).step({ duration: 200 }).translateY(0).step({ duration: 1800 });
     animationDataTwo.translateY(-670).step({ duration: 2000 }).translateY(603).step({ duration: 200 }).translateY(0).step({ duration: 1800 });
 
@@ -113,8 +61,7 @@ Page({
       animationDataTwo: animationDataTwo.export(),
       
     },function(){
-       i++ 
-      if(_this.run && i<3){
+      if(_this.run){
 
         _this.james = setTimeout(function () {
           _this.WXAniamtion()
@@ -130,22 +77,16 @@ Page({
 
   },
   endAnimation(){
-    console.log('==========',this.number)
     let one = -(parseInt(this.number.slice(0, 1))*67)
     let two = -(parseInt(this.number.slice(1, 2))*67)
     let oneTime = parseInt(this.number.slice(0, 1))* 200;
     let twoTime = parseInt(this.number.slice(1, 2))* 200;
-    console.log(one,twoTime,'endAnimation',two,oneTime)
     var animationData = wx.createAnimation({
-      duration: 400, // 默认为400     动画持续时间，单位ms
-      // timingFunction: 'ease-in-out',
-      //transformOrigin: '4px 91px'
+      duration: 400, 
     });
     // 第二个
     var animationCloudData = wx.createAnimation({
-      duration: 400, // 默认为400     动画持续时间，单位ms
-      // duration: 1000, // 默认为400     动画持续时间，单位ms
-      // timingFunction: 'ease-in-out',
+      duration: 400, 
     });
     animationData.translateY(one).step({ duration: oneTime });
     animationCloudData.translateY(two).step({ duration: twoTime });
@@ -160,30 +101,8 @@ Page({
     
   },
   stop(e){
-    this.number = '07'
-    this.run = false;
-    return;
-    console.log('stop',e,e.animationend)
-    let that = this;
-    var animationCloudData = wx.createAnimation({
-      duration: 1000, // 默认为400     动画持续时间，单位ms
-      timingFunction: 'ease-in-out',
-    });
-    animationCloudData.translateY(120).step({ duration: 500 });
-    clearInterval(that.james)
-
-    setTimeout(function(){
-      that.setData({
-        // 导出动画示例
-          //animationData: animationData.export(),
-          animationCloudData: animationCloudData.export(),
-        },function(){
-          console.log('结束动画')
-        })
-    },10000)
-        
-        
-        
+     this.james.stop('07')
+    // this.number = '07'
   },
   
 });
--- src/utils/numAnimate.js
+++ src/utils/numAnimate.js
@@ -1,166 +1,111 @@
+/*
+  点击停止的动画
+  参数传入：{
+  	_this:this指针,
+  	callback:执行stop函数后的回调
 
-var Tween = {
-	Quad: {
-		easeOut: function(t, b, c, d) {
-			return -c * (t /= d) * (t - 2) + b;
-		}
-	}
-}
-var b=0,c=0,d=500,t=0;
-var imgHeight = 120;//单独数字的高度
-var ListHeight = 1200;//循环动画的高度
-var numData = []
-var obj = {}
-var that ;
-var index = 0
-// 活动方法
-var animate = function(numArr,number,_this){
-	that = _this;
-	let len = number.length;
-	numData = dealData(numArr,number)
-	c = numData[index].label*imgHeight;
-	obj = numData[index];
-	Run()
-
-}
-
-var dealData = function(numArr,number){
-	let len = number.length;
-	for (var i = 0; i < len; i++) {
-		numArr[i].label = number.slice(i,i+1);
-		numArr[i].top = 0;
-	}
-	return numArr;
-}
-
-var Run = function(){
-	obj.top = Math.ceil(Tween.Quad.easeOut(t,b,c,d));
-	that.setData({
-		numArr : numData
-	},function(){
-		if(t<d){ 
-			t++; 
-			setTimeout(Run, 1); 
-		}else if(t == d && index < numData.length-1){
-			index++;
-			t=0;
-			c = numData[index].label*imgHeight;
-			obj = numData[index];
-			Run()
-		}
-	})
-	
-	
-}
-
-
-// 分享后方法
-
-var animates = function(numArr,_this,callback){
-	that = _this;
-	numData = dealList(numArr)
-	that.setData({
-		numArr : numData
-	},function(){
-		c = 120*19
-		allRun(callback)
-	})
-	
-}
-var dealList = function(numArr){
-	let len = numArr.length;
-	for (var i = 0; i < len; i++) {
-		numArr[i].oneTop = 0;
-		numArr[i].top = 0;
-		numArr[i].towTop = ListHeight;
-		numArr[i].targetTop = false;
-		numArr[i].targetBox = ''
-	}
-	return numArr;
-}
-let one = 0;
-let tow = ListHeight;
-let moveNum = 0;
+  }
+  
+*/
 // 是否转动
-let move = true
-let moved = [];
-var allRun = function(callback){
-	moveNum  += 10;
-	var top = moveNum
-	let len = numData.length;
-	let index =	Math.ceil(moveNum/ListHeight);
-	let num = index%2 == 1?'one':'tow';//当前所在
-	for (var i = 0; i < len; i++) {
-		if(num === numData[i].targetBox && num === 'one' && moveNum >= numData[i].targetMove){
-			numData[i].oneTop = '-'+numData[i].targetTop;
-			moved[i] = true
-		}else{
-			let oneTop = one-top;
-			numData[i].oneTop =oneTop
-		}
-
-		if(num === numData[i].targetBox && num === 'tow' && moveNum >= numData[i].targetMove){
-			numData[i].towTop = '-'+numData[i].targetTop;
-			moved[i] = true
-		}else{
-			let towTop = tow-top
-			numData[i].towTop = towTop
-		}
-		if(i === 2){
-		}
-		
+let run = true
+let wx_this;
+let wx_that;
+export class demoAnimates {
+	constructor(parameter) {
+		this.callback = parameter.callback;
+		wx_this = parameter._this;
+		wx_that = this;
+		this.WXAniamtion();
 	}
-	that.setData({
-		numArr : numData
-	},function(){
-		if(moveNum>ListHeight){
-			if(num === 'one'){
-				tow = index *ListHeight
-			}else{
-				one = index * ListHeight
+	WXAniamtion() {
+		let that = this;
+		var animationData = wx.createAnimation({
+			duration: 400,
+		});
+		var animationDataTwo = wx.createAnimation({
+			duration: 400,
+		});
+		// 第二个
+		var animationCloudData = wx.createAnimation({
+			duration: 400,
+		});
+		animationData.translateY(-670).step({
+			duration: 2000
+		}).translateY(603).step({
+			duration: 200
+		}).translateY(0).step({
+			duration: 1800
+		});
+		animationDataTwo.translateY(-670).step({
+			duration: 2000
+		}).translateY(603).step({
+			duration: 200
+		}).translateY(0).step({
+			duration: 1800
+		});
+
+		animationCloudData.translateY(-670).step({
+			duration: 2000
+		}).translateY(-1340).step({
+			duration: 2000
+		}).translateY(0).step({
+			duration: 10
+		});
+		wx_this.setData({
+			animationCloudData: animationCloudData.export(),
+			animationDataOne: animationData.export(),
+			animationDataTwo: animationDataTwo.export(),
+
+		}, function() {
+			if (run) {
+
+				setTimeout(function() {
+					wx_that.WXAniamtion()
+				}, 4100)
+
+			} else {
+				setTimeout(function() {
+					wx_that.endAnimation()
+				}, 4000)
+
 			}
-		}
-		if(move){
-			window.requestAnimFrame = (function () {
-	            return window.requestAnimationFrame ||
-	                window.webkitRequestAnimationFrame ||
-	                window.mozRequestAnimationFrame ||
-	                function (callback) {
-	                    window.setTimeout(callback, 6000 / 60);
-	                };
-	        })();
+		})
 
-
-
-			requestAnimFrame(()=>{allRun(callback)});
-			if(moved.length == numData.length){
-				move = false
-			} 
-		}else{
-			callback(that)
-		}
-	})
-}
-var stoped = true
-var stop = function(num){
-	console.log('==========')
-	if(!stoped){
-		return
 	}
-	stoped = false;
-	console.log('---------')
-	let index =	Math.ceil(moveNum/ListHeight);
-	let box = index%2 == 1?'tow':'one';//当前所在
-	let len = num.length;
-	for (var i = 0; i < len; i++) {
-		let number = parseInt(num.slice(i,i+1));
-		numData[i].targetMove = index*ListHeight+number*imgHeight;
-		numData[i].targetTop = number*imgHeight;
-		numData[i].targetBox = box;
+	stop(num) {
+		if (!run) {
+			return
+		}
+		run = false;
+		this.number = num
 	}
-}
-module.exports = {
-	animate:animate,
-	animates:animates,
-	stop:stop
+	endAnimation() {
+		let one = -(parseInt(wx_that.number.slice(0, 1)) * 67)
+		let two = -(parseInt(wx_that.number.slice(1, 2)) * 67)
+		let oneTime = parseInt(wx_that.number.slice(0, 1)) * 200;
+		let twoTime = parseInt(wx_that.number.slice(1, 2)) * 200;
+		var animationData = wx.createAnimation({
+			duration: 400,
+		});
+		// 第二个
+		var animationCloudData = wx.createAnimation({
+			duration: 400,
+		});
+		animationData.translateY(one).step({
+			duration: oneTime
+		});
+		animationCloudData.translateY(two).step({
+			duration: twoTime
+		});
+
+		wx_this.setData({
+			animationDataOne: animationData.export(),
+			animationDataTwo: animationCloudData.export(),
+		}, function() {
+			wx_that.callback(wx_this)
+		})
+	}
+
+
 }
